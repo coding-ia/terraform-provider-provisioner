7d7bd66732d454baae0b0f2dfbd1707391645db6
bjunker <bjunker99@gmail.com>
2023-12-07 23:28:42 -0800

    Update start-localstack.sh

--- scripts/start-localstack.sh
+++ scripts/start-localstack.sh
@@ -6,7 +6,7 @@ export AWS_SECRET_ACCESS_KEY=test
 export AWS_DEFAULT_REGION=us-east-2
 export AWS_SESSION_TOKEN=test
 
-mkdir ~/.aws && cp fake.aws ~/.aws/
+mkdir ~/.aws && cp ./scripts/fake.aws ~/.aws/credentials
 docker-compose -f ./scripts/docker-compose.yaml up -d
 sleep 30s
 aws --endpoint-url=http://localhost:4566 sns create-topic --name go-test-topic
